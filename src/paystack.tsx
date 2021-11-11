586264deef8929a7dfffb435d8620bccfdd9746e
Toni E <tons613@ymail.com>
2021-11-11 12:09:35 +0100

    tsx

--- src/paystack.tsx
+++ src/paystack.tsx
@@ -41,7 +41,7 @@ const Paystack: React.ForwardRefRenderFunction<React.ReactNode, PayStackProps> =
     const options = [];
     if (metadata) {
       for (let key in metadata) {
-        let option = metadata[key];
+        var option = metadata[key];
         if (option) {
           options.push(`${key}:'${option}'`);
         }
